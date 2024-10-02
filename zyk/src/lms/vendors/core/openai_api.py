175fbbf2c14de5293ce7fbfd00ebe4f86b05910e
Josh Purtell <jmvpurtell@gmail.com>
2024-10-02 13:39:35 -0700

    Save

--- zyk/src/lms/vendors/core/openai_api.py
+++ zyk/src/lms/vendors/core/openai_api.py
@@ -33,7 +33,7 @@ class OpenAIStructuredOutputClient(OpenAIStandard):
     async def _hit_api_async_structured_output(self, model: str, messages: List[Dict[str, Any]], response_model: BaseModel, temperature: float, use_ephemeral_cache_only: bool = False) -> str:
         lm_config = {"temperature": temperature, "response_model": response_model}
         used_cache_handler = get_cache_handler(use_ephemeral_cache_only=use_ephemeral_cache_only)
-        cache_result = used_cache_handler.hit_cache(model, messages, lm_config=lm_config)
+        cache_result = used_cache_handler.hit_managed_cache(model, messages, lm_config=lm_config)
         if cache_result:
             return cache_result["response"] if isinstance(cache_result, dict) else cache_result
         output = await self.async_client.beta.chat.completions.parse(
@@ -48,7 +48,7 @@ class OpenAIStructuredOutputClient(OpenAIStandard):
     def _hit_api_sync_structured_output(self, model: str, messages: List[Dict[str, Any]], response_model: BaseModel, temperature: float, use_ephemeral_cache_only: bool = False) -> str:
         lm_config = {"temperature": temperature, "response_model": response_model}
         used_cache_handler = get_cache_handler(use_ephemeral_cache_only=use_ephemeral_cache_only)
-        cache_result = used_cache_handler.hit_cache(model, messages, lm_config=lm_config)
+        cache_result = used_cache_handler.hit_managed_cache(model, messages, lm_config=lm_config)
         if cache_result:
             return cache_result["response"] if isinstance(cache_result, dict) else cache_result
         output = self.sync_client.beta.chat.completions.parse(
