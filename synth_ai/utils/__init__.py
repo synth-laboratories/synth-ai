75066d479cfc22a0dfd9d8b5d1b4e785f8067f54
Zangus <mike@usesynth.ai>
2025-10-24 12:09:47 -0700

    cli visualizations, removed functions

--- synth_ai/utils/__init__.py
+++ synth_ai/utils/__init__.py
@@ -2,10 +2,13 @@ from __future__ import annotations
 
 import importlib
 
-from .files import write_text
-from .formatting import format_currency, format_int, safe_str
-from .http import AsyncHttpClient, HTTPError, http_request, sleep
-from .keys import ensure_key
+from .cli_visualizations import (
+    ensure_key,
+    ensure_required_args,
+    key_preview,
+    print_next_step,
+)
+from .http import AsyncHttpClient, HTTPError, http_request
 from .modal import (
     ensure_modal_installed,
     ensure_task_app_ready,
@@ -14,10 +17,7 @@ from .modal import (
     is_modal_public_url,
     normalize_endpoint_url,
 )
-from .print_next_step import print_next_step
 from .process import ensure_local_port_available, popen_capture, popen_stream, popen_stream_capture
-from .user_input import ensure_required_args
-from .secrets import key_preview
 from .sqld import SQLD_VERSION, find_sqld_binary, install_sqld
 from .user_config import (
     USER_CONFIG_PATH,
@@ -28,14 +28,9 @@ from .user_config import (
 )
 
 __all__ = [
-    "write_text",
-    "format_currency",
-    "format_int",
-    "safe_str",
     "AsyncHttpClient",
     "HTTPError",
     "http_request",
-    "sleep",
     "ensure_local_port_available",
     "popen_capture",
     "popen_stream",
@@ -60,6 +55,6 @@ __all__ = [
     "ensure_key",
 ]
 
-task_app_state = importlib.import_module("synth_ai._utils.task_app_state")
+task_app_state = importlib.import_module("synth_ai.utils.task_app_state")
 
 __all__.append("task_app_state")
