6a0fde99169bc26589d93c94ec22b47fb0b173e9
Zangus <mike@usesynth.ai>
2025-10-27 13:25:51 -0700

    updated exports

--- synth_ai/utils/__init__.py
+++ synth_ai/utils/__init__.py
@@ -20,37 +20,35 @@ from .task_app_discovery import AppChoice, discover_eval_config_paths, select_ap
 from .task_app_env import (
     ensure_env_credentials,
     ensure_port_free,
-    hydrate_user_environment,
     preflight_env_key,
 )
-from .user_config import (
-    USER_CONFIG_PATH,
-    load_user_config,
-    load_user_env,
-    save_user_config,
-    update_user_config,
-)
+from .user_config import USER_CONFIG_PATH, load_user_config, load_user_env, save_user_config, update_user_config
 
-__all__ = [
+_BASE_EXPORTS = (
     "AppChoice",
     "AsyncHttpClient",
     "HTTPError",
-    "http_request",
+    "PROD_BASE_URL_DEFAULT",
     "PromptedChoiceOption",
     "PromptedChoiceType",
-    "PROD_BASE_URL_DEFAULT",
+    "SQLD_VERSION",
+    "USER_CONFIG_PATH",
     "discover_eval_config_paths",
     "ensure_env_credentials",
     "ensure_local_port_available",
     "ensure_modal_installed",
-    "ensure_task_app_ready",
     "ensure_port_free",
+    "ensure_task_app_ready",
     "find_asgi_apps",
-    "is_local_demo_url",
-    "is_modal_public_url",
+    "find_sqld_binary",
     "get_backend_from_env",
     "get_learning_v2_base_url",
-    "hydrate_user_environment",
+    "http_request",
+    "install_sqld",
+    "is_local_demo_url",
+    "is_modal_public_url",
+    "load_user_config",
+    "load_user_env",
     "mask_str",
     "normalize_endpoint_url",
     "popen_capture",
@@ -59,24 +57,18 @@ __all__ = [
     "preflight_env_key",
     "print_next_step",
     "resolve_env_var",
-    "SQLD_VERSION",
-    "find_sqld_binary",
-    "install_sqld",
-    "USER_CONFIG_PATH",
-    "load_user_config",
-    "load_user_env",
     "save_user_config",
-    "update_user_config",
     "select_app_choice",
+    "task_app_state",
+    "update_user_config",
     "write_env_var_to_dotenv",
     "write_env_var_to_json",
-]
+)
 
 task_app_state = importlib.import_module("synth_ai.utils.task_app_state")
 
-__all__.append("task_app_state")
-
-for _name in getattr(task_app_state, "__all__", []):
+_TASK_APP_STATE_EXPORTS = tuple(getattr(task_app_state, "__all__", ()))
+for _name in _TASK_APP_STATE_EXPORTS:
     globals()[_name] = getattr(task_app_state, _name)
-    if _name not in __all__:
-        __all__.append(_name)
+
+__all__ = (*_BASE_EXPORTS, *_TASK_APP_STATE_EXPORTS)
