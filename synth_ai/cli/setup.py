4ec86f9c56c11c875b6c7a49fed02a910f6284ac
Zangus <mike@usesynth.ai>
2025-10-28 12:32:16 -0700

    added local option

--- synth_ai/cli/setup.py
+++ synth_ai/cli/setup.py
@@ -1,12 +1,21 @@
+"""Instructions on Docs → https://usesynth.ai/cli-cmds/setup"""
+
 import click
 from synth_ai.auth.credentials import fetch_credentials_from_web_browser_session
 
 
 @click.command("setup")
 @click.option(
-    "--dev",
+    "--local",
     is_flag=True,
-    help="Use the development environment instead of production.",
+    help="Load your credentials from your local machine"
 )
-def setup_cmd(dev: bool) -> None:
-    fetch_credentials_from_web_browser_session(prod=not dev)
+@click.option(
+    "--dev",
+    is_flag=True
+)
+def setup_cmd(local: bool, dev: bool) -> None:
+    fetch_credentials_from_web_browser_session(
+        browser=not local,
+        prod=not dev
+    )
