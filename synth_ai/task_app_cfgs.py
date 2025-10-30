ccdb7f4140e7d4f6b713ed29862504e5b5afe183
Zangus <mike@usesynth.ai>
2025-10-29 19:16:32 -0700

    created config classes for deploying task apps

---
+++ synth_ai/task_app_cfgs.py
@@ -0,0 +1,19 @@
+from typing import Literal, Optional
+from pydantic import BaseModel
+from pathlib import Path
+
+
+class ModalTaskAppConfig(BaseModel):
+    task_app_path: Path
+    modal_app_path: Path
+    modal_bin_path: Path
+    cmd_arg: Literal["deploy", "serve"] = "deploy"
+    task_app_name: Optional[str] = None
+    dry_run: bool = False
+    
+
+class LocalTaskAppConfig(BaseModel):
+    task_app_path: Path
+    trace: bool = True
+    host: str = "127.0.0.1"
+    port: int = 8000
