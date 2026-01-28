5f4fd9cd3d7a2c26ca30b0fd649d796fe2e9c017
Josh Purtell <jmvpurtell@gmail.com>
2026-01-28 01:20:36 -0800

    export run_server_background

--- synth_ai/sdk/task/server.py
+++ synth_ai/sdk/task/server.py
@@ -2,6 +2,6 @@
 
 from __future__ import annotations
 
-from synth_ai.sdk.localapi._impl.server import RubricBundle
+from synth_ai.sdk.localapi._impl.server import RubricBundle, run_server_background
 
-__all__ = ["RubricBundle"]
+__all__ = ["RubricBundle", "run_server_background"]
