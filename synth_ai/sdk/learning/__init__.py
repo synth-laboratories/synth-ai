a2b454c3da90115f0a2ff543ffec9bf2654d5b63
Josh Purtell <jmvpurtell@gmail.com>
2026-01-28 00:26:11 -0800

    add sdk learning shims

---
+++ synth_ai/sdk/learning/__init__.py
@@ -0,0 +1,7 @@
+"""Backward-compatible learning namespace."""
+
+from __future__ import annotations
+
+from synth_ai.sdk.optimization.clients import PromptLearningClient
+
+__all__ = ["PromptLearningClient"]
