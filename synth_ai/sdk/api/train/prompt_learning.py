8204bba0ddd871c21248f619f1e0a9042e38df03
Josh Purtell <jmvpurtell@gmail.com>
2026-01-28 00:49:14 -0800

    export PromptLearningResult

--- synth_ai/sdk/api/train/prompt_learning.py
+++ synth_ai/sdk/api/train/prompt_learning.py
@@ -6,5 +6,10 @@ from synth_ai.sdk.optimization.internal.prompt_learning import (  # noqa: F401
     PromptLearningJob,
     PromptLearningJobConfig,
 )
+from synth_ai.sdk.optimization.models import PromptLearningResult  # noqa: F401
 
-__all__ = ["PromptLearningJob", "PromptLearningJobConfig"]
+__all__ = [
+    "PromptLearningJob",
+    "PromptLearningJobConfig",
+    "PromptLearningResult",
+]
