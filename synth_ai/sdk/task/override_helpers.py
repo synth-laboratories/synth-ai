d03155c9150b3c660de0606875d176dd46d53ec4
Josh Purtell <jmvpurtell@gmail.com>
2026-01-28 00:18:44 -0800

    add task override_helpers shim

---
+++ synth_ai/sdk/task/override_helpers.py
@@ -0,0 +1,15 @@
+"""Backward-compatible overrides helpers for synth_ai.sdk.task."""
+
+from __future__ import annotations
+
+from synth_ai.sdk.localapi._impl.override_helpers import (  # noqa: F401
+    AgentType,
+    apply_context_overrides,
+    get_applied_env_vars,
+)
+
+__all__ = [
+    "AgentType",
+    "apply_context_overrides",
+    "get_applied_env_vars",
+]
