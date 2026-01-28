b52d024f480d5cb09652b297b0cbfb8902f63c62
Josh Purtell <jmvpurtell@gmail.com>
2026-01-28 00:25:29 -0800

    export graphgen taskset loaders

--- synth_ai/sdk/api/train/graphgen.py
+++ synth_ai/sdk/api/train/graphgen.py
@@ -11,9 +11,15 @@ from synth_ai.sdk.optimization.internal.graphgen import (
 from synth_ai.sdk.optimization.internal.graphgen import (
     GraphEvolveSubmitResult as GraphGenSubmitResult,
 )
+from synth_ai.sdk.optimization.internal.graphgen_models import (  # noqa: F401
+    load_graphgen_taskset,
+    parse_graphgen_taskset,
+)
 
 __all__ = [
     "GraphGenJob",
     "GraphGenJobResult",
     "GraphGenSubmitResult",
+    "load_graphgen_taskset",
+    "parse_graphgen_taskset",
 ]
