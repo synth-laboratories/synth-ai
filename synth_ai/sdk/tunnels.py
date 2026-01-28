9bb2961594a969c08c97c84d12a39406cc21d587
Josh Purtell <jmvpurtell@gmail.com>
2026-01-28 00:27:06 -0800

    export tunnel backend helpers

--- synth_ai/sdk/tunnels.py
+++ synth_ai/sdk/tunnels.py
@@ -4,7 +4,14 @@ from __future__ import annotations
 
 from synth_ai.core.tunnels import (  # noqa: F401
     PortConflictBehavior,
+    TunnelBackend,
+    TunneledLocalAPI,
     acquire_port,
 )
 
-__all__ = ["PortConflictBehavior", "acquire_port"]
+__all__ = [
+    "PortConflictBehavior",
+    "TunnelBackend",
+    "TunneledLocalAPI",
+    "acquire_port",
+]
